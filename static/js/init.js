9de5fd31c777384e74a363cdf09e50b572025366
carlosblancomarketing <carlosblancosierra@gmail.com>
2021-09-16 00:37:06 +0200

    Style: Nav Dropdown style

--- static/js/init.js
+++ static/js/init.js
@@ -3,5 +3,7 @@ M.AutoInit();
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, {'height' : 200, 'indicators' : true});
-});
 
+    var navDropdown = document.querySelectorAll('.dropdown-trigger-main');
+    var instances = M.Dropdown.init(navDropdown, ({alignment:'left', 'hover':true, 'constrainWidth':false}));
+});

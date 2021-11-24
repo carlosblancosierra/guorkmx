7ca99d395c60654817f8f6eadd7fe61ab12a66dc
carlosblancomarketing <carlosblancosierra@gmail.com>
2021-11-24 13:26:22 +0100

    Fix: Recursos navbar link and image

--- recursos/views.py
+++ recursos/views.py
@@ -16,14 +16,14 @@ def home_page(request):
                     "desc": "5 tácticas avanzadas de capacidad de entrega del correo electrónico para ayudarte a llegar "
                             "a la bandeja de entrada y aumentar la participación.",
                     "url": "/recursos/capacidad-de-entrega-del-email",
-                    "image": "/static/images/recursos/icons/Email-Campaign-Creation.png"
+                    "image": "images/recursos/icons/Email-Campaign-Creation.png"
                 },
                 {
                     "title": "Creación de Campañas de Emailing",
                     "desc": "Asegúrate de que tus campañas de emailing se envíen, reciban clics, proporcionen valor y "
                             "vendan.",
                     "url": "/recursos/creacion-de-campanas-de-email",
-                    "image": "/static/images/recursos/icons/Email-Deliverability.png"
+                    "image": "images/recursos/icons/Email-Deliverability.png"
                 },
 
             ]
@@ -37,14 +37,14 @@ def home_page(request):
                     "desc": "Una vez que desarrolles campañas de retargeting basadas en el comportamiento anterior de "
                             "los usuarios, generar mayores ingresos será muy sencillo.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Estrategias de Facebook Ads",
                     "desc": "Como marketero, debes pensar en Facebook de manera diferente. Los consumidores no están en "
                             "Facebook para ver anuncios y es importante recordar que sigue siendo una plataforma social.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
 
             ]
@@ -58,27 +58,28 @@ def home_page(request):
                     "desc": "El blog debe aportar valor a tus clientes ayudandolos a resolver sus problemas. Hay muchas "
                             "formas de hacer esto, aquí te ayudaremos a crear una estrategia paso a paso. ",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Distribución de Contenido",
                     "desc": "El reto más grande es atraer tráfico a tus contenidos. Para ello, debes distribuirlos a "
                             "través de los canales adecuados, ya sea de forma orgánica o con publicidad pagada. ",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Redacción de Contenidos",
                     "desc": "El objetivo de una buena redacción es generar una cierta expectativa o interés en la "
                             "audiencia para que puedas vender más. Aquí te decimos cómo.",
                     "url": "#",
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Contenido para el Top of the Funnel",
                     "desc": "Aprende a captar la atención de nuevos usuarios y comienza a generar más clientes "
                             "potenciales para tu negocio.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
 
             ]
@@ -92,27 +93,27 @@ def home_page(request):
                     "desc": "Si cada pieza de contenido que produces está optimizada y es valiosa para tus usuarios, "
                             "la clasificación en los motores de búsqueda será un problema menor.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Auditoría SEO",
                     "desc": "Nadie quiere informar sobre métricas irrelevantes. Descubre el cómo y el por qué de los "
                             "informes de SEO.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Herramientas de Investigación de Palabras Clave",
                     "desc": "Aprende a hacer una investigación de palabras clave y crear contenido que sea importante "
                             "para tus clientes.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "SEO Backlinks",
                     "desc": "Con más backlinks, conducirás más y mejor tráfico a tu sitio, ¡lo que significa más dinero!",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
 
             ]
@@ -126,26 +127,26 @@ def home_page(request):
                     "desc": "Con las herramientas y estrategias correctas puedes persuadir a los prospectos para que "
                             "compren más y más rápido. ",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "¿Qué es Copywriting?",
                     "desc": "Cuando se trata de alcanzar más clientes, no hay nada más importante que el mensaje.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Copywriting Efectivo",
                     "desc": "Una investigación efectiva y el conocimiento de la voz de tu marca te ayudarán a producir "
                             "un ‘copy’ efectivo",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Propuesta Única de Venta",
                     "desc": "Tu propuesta única de venta debe ser sólida y debe ser clara en cualquier ‘copy’ de ventas.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
 
             ]
@@ -159,28 +160,28 @@ def home_page(request):
                     "desc": "Social Listening es la clave del éxito cuando se trata de Redes Sociales. "
                             "Te explicamos porque",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Community Management",
                     "desc": "La función del Community Manager es crear un entorno saludable para que los miembros se "
                             "conecten e interactúen.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Construir una Comunidad Digital",
                     "desc": "Descubre cómo trabajar en equipo puede llevar tus estrategias de Marketing Digital "
                             "al siguiente nivel.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "Qué es un Plan de Ejecución",
                     "desc": "Haz crecer tu negocio a medida que sigues el paso a paso que te llevarán a ser más "
                             "productivo.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
 
             ]
@@ -193,14 +194,14 @@ def home_page(request):
                     "title": "Personalización del Website",
                     "desc": "El objetivo final de la personalización es convertir tu sitio web en tu mejor vendedor.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
                 {
                     "title": "A/B Testing",
                     "desc": "Descubre por qué los A/B Test son una de las mejores formas de saber qué resuena con tus "
                             "clientes.",
                     "url": "#",
-                    "image": "/static/images/recursos/placeholder.png"
+                    "image": "images/recursos/placeholder.png"
                 },
 
             ]
